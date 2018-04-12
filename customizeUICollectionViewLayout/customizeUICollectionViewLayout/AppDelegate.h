ffaf7ec606fb6b82d346f45d64220cefeec1a02d
mali <mali@malideair.lan>
2018-04-12 22:22:26 +0800

    2018.4.12初始化项目

---
+++ customizeUICollectionViewLayout/customizeUICollectionViewLayout/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  customizeUICollectionViewLayout
+//
+//  Created by mali on 2018/4/12.
+//  Copyright © 2018年 DreamBlue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ customizeUICollectionViewLayout/customizeUICollectionViewLayout/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  customizeUICollectionViewLayout
+//
+//  Created by mali on 2018/4/12.
+//  Copyright © 2018年 DreamBlue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
